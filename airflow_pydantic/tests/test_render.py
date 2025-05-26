55cb0cc8233de3a743e2e9f4d4a4640dd2d35053
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-05-26 16:20:41 -0400

    Start on DAG rendering

--- airflow_pydantic/tests/test_render.py
+++ airflow_pydantic/tests/test_render.py
@@ -1,3 +1,6 @@
+from airflow_pydantic import Dag
+
+
 class TestRender:
     def test_render_operator(self, python_operator):
         imports, globals_, task = python_operator.render()
@@ -6,9 +9,10 @@ class TestRender:
             "from airflow_pydantic.tests.test_operators import test",
         ]
         assert globals_ == []
-        assert task == [
-            "PythonOperator(python_callable=test, op_args=['test'], op_kwargs={'test': 'test'}, templates_dict={'test': 'test'}, templates_exts=['.sql', '.hql'], show_return_value_in_logs=True, task_id='test_python_operator')"
-        ]
+        assert (
+            task
+            == "PythonOperator(python_callable=test, op_args=['test'], op_kwargs={'test': 'test'}, templates_dict={'test': 'test'}, templates_exts=['.sql', '.hql'], show_return_value_in_logs=True, task_id='test_python_operator')"
+        )
 
     def test_render_operator_dag_from_context(self, python_operator):
         imports, globals_, task = python_operator.render(dag_from_context=True)
@@ -17,9 +21,10 @@ class TestRender:
             "from airflow_pydantic.tests.test_operators import test",
         ]
         assert globals_ == []
-        assert task == [
-            "PythonOperator(python_callable=test, op_args=['test'], op_kwargs={'test': 'test'}, templates_dict={'test': 'test'}, templates_exts=['.sql', '.hql'], show_return_value_in_logs=True, task_id='test_python_operator', dag=dag)"
-        ]
+        assert (
+            task
+            == "PythonOperator(python_callable=test, op_args=['test'], op_kwargs={'test': 'test'}, templates_dict={'test': 'test'}, templates_exts=['.sql', '.hql'], show_return_value_in_logs=True, task_id='test_python_operator', dag=dag)"
+        )
 
     def test_render_operator_bash(self, bash_operator):
         imports, globals_, task = bash_operator.render()
@@ -28,9 +33,10 @@ class TestRender:
             "from airflow_pydantic.tests.test_operators import test",
         ]
         assert globals_ == []
-        assert task == [
-            "BashOperator(bash_command='test', env={'test': 'test'}, append_env=True, output_encoding='utf-8', skip_exit_code=True, skip_on_exit_code=99, cwd='test', output_processor=test, task_id='test_bash_operator')",
-        ]
+        assert (
+            task
+            == "BashOperator(bash_command='test', env={'test': 'test'}, append_env=True, output_encoding='utf-8', skip_exit_code=True, skip_on_exit_code=99, cwd='test', output_processor=test, task_id='test_bash_operator')"
+        )
 
     def test_render_operator_ssh(self, ssh_operator):
         imports, globals_, task = ssh_operator.render()
@@ -38,54 +44,27 @@ class TestRender:
             "from airflow.providers.ssh.operators.ssh import SSHOperator",
         ]
         assert globals_ == []
-        assert task == [
-            "SSHOperator(do_xcom_push=True, ssh_conn_id='test', command='test', get_pty=True, task_id='test_ssh_operator', timeout=10, env={'test': 'test'})",
-        ]
+        assert (
+            task
+            == "SSHOperator(do_xcom_push=True, ssh_conn_id='test', command='test', get_pty=True, task_id='test_ssh_operator', timeout=10, env={'test': 'test'})"
+        )
 
-    # def test_render(self):
-    #     dag = Dag(
-    #         description="",
-    #         schedule="* * * * *",
-    #         start_date=datetime.today(),
-    #         end_date=datetime.today(),
-    #         max_active_tasks=1,
-    #         max_active_runs=1,
-    #         default_view="grid",
-    #         orientation="LR",
-    #         catchup=False,
-    #         is_paused_upon_creation=True,
-    #         tags=["a", "b"],
-    #         dag_display_name="test",
-    #         enabled=True,
-    #         dag_id="a-dag",
-    #         default_args=None,
-    #         tasks={
-    #             "task1": PythonOperator(
-    #                 python_callable="airflow_pydantic.tests.test_operators.test",
-    #                 op_args=["test"],
-    #                 op_kwargs={"test": "test"},
-    #                 templates_dict={"test": "test"},
-    #                 templates_exts=[".sql", ".hql"],
-    #                 show_return_value_in_logs=True,
-    #             ),
-    #             "task2": BashOperator(
-    #                 bash_command="test",
-    #                 env={"test": "test"},
-    #                 append_env=True,
-    #                 output_encoding="utf-8",
-    #                 skip_exit_code=True,
-    #                 skip_on_exit_code=99,
-    #                 cwd="test",
-    #                 output_processor="airflow_pydantic.tests.test_operators.test",
-    #             ),
-    #             "task3": SSHOperator(
-    #                 ssh_conn_id="test",
-    #                 command="test",
-    #                 do_xcom_push=True,
-    #                 timeout=10,
-    #                 get_pty=True,
-    #                 env={"test": "test"},
-    #             ),
-    #         },
-    #     )
-    #     assert dag.render() == ""
+    def test_render(self, dag):
+        assert isinstance(dag, Dag)
+        assert (
+            dag.render()
+            == """# Generated by airflow-config
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow_pydantic.tests.test_operators import test
+
+
+with DAG(dag_id='a-dag') as dag:
+    # Tasks
+    task1 = PythonOperator(python_callable=test, op_args=['test'], op_kwargs={'test': 'test'}, templates_dict={'test': 'test'}, templates_exts=['.sql', '.hql'], show_return_value_in_logs=True, task_id='test_python_operator', dag=dag)
+    task2 = BashOperator(bash_command='test', env={'test': 'test'}, append_env=True, output_encoding='utf-8', skip_exit_code=True, skip_on_exit_code=99, cwd='test', output_processor=test, task_id='test_bash_operator', dag=dag)
+    task3 = SSHOperator(do_xcom_push=True, ssh_conn_id='test', command='test', get_pty=True, task_id='test_ssh_operator', timeout=10, env={'test': 'test'}, dag=dag)
+"""
+        )
