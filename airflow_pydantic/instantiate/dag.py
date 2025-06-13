96792cc768714078ebd859fb0b2c191a0b2fc557
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-06-13 12:45:21 -0400

    Remove unneeded context manager

--- airflow_pydantic/instantiate/dag.py
+++ airflow_pydantic/instantiate/dag.py
@@ -13,18 +13,17 @@ class DagInstantiateMixin:
         if not dag_instance:
             dag_instance = DAG(dag_id=self.dag_id, **self.model_dump(exclude_none=True, exclude=["type_", "tasks", "dag_id"]), **kwargs)
 
-        with dag_instance:
-            task_instances = {}
+        task_instances = {}
 
-            # first pass, instantiate all
-            for task_id, task in self.tasks.items():
-                if not task_id:
-                    raise ValueError("task_id must be set to instantiate a task")
-                task_instances[task_id] = task.instantiate(dag=dag_instance, **kwargs)
+        # first pass, instantiate all
+        for task_id, task in self.tasks.items():
+            if not task_id:
+                raise ValueError("task_id must be set to instantiate a task")
+            task_instances[task_id] = task.instantiate(dag=dag_instance, **kwargs)
 
-            # second pass, set dependencies
-            for task_id, task in self.tasks.items():
-                for dep in task.dependencies:
-                    task_instances[dep] >> task_instances[task_id]
+        # second pass, set dependencies
+        for task_id, task in self.tasks.items():
+            for dep in task.dependencies:
+                task_instances[dep] >> task_instances[task_id]
 
         return dag_instance
