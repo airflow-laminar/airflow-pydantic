a904bdf3a0f29880947ca429a6b5acf2b2909fa9
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-06-13 16:25:14 -0400

    Render dag args, apply autoformatting

--- airflow_pydantic/render/task.py
+++ airflow_pydantic/render/task.py
@@ -1,13 +1,48 @@
 import ast
-from types import FunctionType, MethodType
 from typing import Dict
 
 from pkn.pydantic import serialize_path_as_string
 
-from ..utils import SSHHook
-from .utils import RenderedCode
+from .utils import RenderedCode, _get_parts_from_value
 
-__all__ = ("TaskRenderMixin",)
+__all__ = (
+    "render_base_task_args",
+    "TaskRenderMixin",
+)
+
+
+def render_base_task_args(self, raw: bool = False, **kwargs: Dict[str, str]) -> RenderedCode:
+    # Extract the importable from the operator path
+    imports = []
+    globals_ = []
+
+    args = {**self.model_dump(exclude_none=True, exclude=["type_"]), **kwargs}
+    for k, v in args.items():
+        new_imports, value = _get_parts_from_value(k, v)
+        if new_imports:
+            imports.extend(new_imports)
+        if isinstance(value, ast.AST):
+            # If the value is already an AST node, we can use it directly
+            args[k] = value
+        else:
+            # Otherwise, we need to convert it to an AST node
+            args[k] = ast.Constant(value=value)
+
+    # Return dictionary literal of args
+    value = ast.Dict(
+        keys=[ast.Constant(value=k) for k in args.keys()],
+        values=[v if isinstance(v, ast.AST) else ast.Constant(value=v) for v in args.values()],
+    )
+    if not raw:
+        # If not raw, we need to convert the imports and inside_dag to a string representation
+        imports = [ast.unparse(i) for i in imports]
+        globals_ = [ast.unparse(i) for i in globals_]
+        value = ast.unparse(value)
+    return (
+        imports,
+        globals_,
+        value,
+    )
 
 
 class TaskRenderMixin:
@@ -22,44 +57,15 @@ class TaskRenderMixin:
 
         args = {**self.model_dump(exclude_none=True, exclude=["type_", "operator", "dependencies"]), **kwargs}
         for k, v in args.items():
-            # Handle ssh_hook specifically
-            if k in ("ssh_hook", "python_callable", "output_processor"):
-                from airflow.providers.ssh.hooks.ssh import SSHHook as BaseSSHHook
-
-                if isinstance(v, BaseSSHHook):
-                    # Add SSHHook to imports
-                    import_, name = serialize_path_as_string(v).rsplit(".", 1)
-                    imports.append(ast.ImportFrom(module=import_, names=[ast.alias(name=name)], level=0))
-
-                    # Add SSHHook builder to args
-                    call = ast.Call(
-                        func=ast.Name(id=name, ctx=ast.Load()),
-                        args=[],
-                        keywords=[],
-                    )
-                    for arg_name in SSHHook.__metadata__[0].__annotations__:
-                        arg_value = getattr(v, arg_name, None)
-                        if arg_value is None:
-                            continue
-                        if isinstance(arg_value, (str, int, float, bool)):
-                            # If the value is a primitive type, we can use ast.Constant
-                            # NOTE: all types in SSHHook are primitives
-                            call.keywords.append(ast.keyword(arg=arg_name, value=ast.Constant(value=arg_value)))
-                        else:
-                            raise TypeError(f"Unsupported type for SSHHook argument '{arg_name}': {type(arg_value)}")
-                    args[k] = call
-
-                elif isinstance(v, (MethodType, FunctionType)):
-                    # If the field is an ImportPath or CallablePath, we need to serialize it as a string and add it to the imports
-                    import_, name = serialize_path_as_string(v).rsplit(".", 1)
-                    imports.append(ast.ImportFrom(module=import_, names=[ast.alias(name=name)], level=0))
-
-                    # Now swap the value in the args with the name
-                    if k in ("ssh_hook",):
-                        # For python_callable and output_processor, we need to use the name directly
-                        args[k] = ast.Call(func=ast.Name(id=name, ctx=ast.Load()), args=[], keywords=[])
-                    else:
-                        args[k] = ast.Name(id=name, ctx=ast.Load())
+            import_, value = _get_parts_from_value(k, v)
+            if import_:
+                imports.append(import_)
+            if isinstance(value, ast.AST):
+                # If the value is already an AST node, we can use it directly
+                args[k] = value
+            else:
+                # Otherwise, we need to convert it to an AST node
+                args[k] = ast.Constant(value=value)
 
         inside_dag = ast.Call(
             func=ast.Name(id=operator_name, ctx=ast.Load()),
